83c1ba7511f1ded8c8cfa1e725ec7e533bdd5fd7
Subhin Hwang <107633586+Surihub@users.noreply.github.com>
2024-06-06 15:20:08 +0000

    feat: 급식 secrets.toml로 수정

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,9 +3,6 @@ import requests
 import json
 import datetime
 import pandas as pd
-from modules import find_my_school, give_me_meal
-
-neis_key = "f63baaa8bb1b4b0fa0b1b41d3a8747f8"
 
 # 학교 조회하는 함수
 @st.cache_data
@@ -56,32 +53,3 @@ def give_me_meal(district_code, school_code, date):
         st.error("정보를 불러올 수 없습니다. 날짜를 다시 확인해주세요! 급식이 없던 날 같아요.")
         menu = []
     return menu
-
-# Streamlit 앱 설정
-st.title("😍급식타임")
-
-url = "https://open.neis.go.kr/hub/schoolInfo"
-
-st.subheader("학교 조회")
-school_input = st.text_input("학교이름을 입력해주세요.")
-
-if school_input:
-    # 1. 학교 먼저 찾기
-    schools_by_district = find_my_school(school_input, url)
-    if schools_by_district:
-        district = st.radio("학교가 소속된 교육청을 선택해주세요", options=schools_by_district.keys())
-        school_name = schools_by_district[district][0]
-        school_code = schools_by_district[district][1]
-        district_code = schools_by_district[district][2]
-
-        st.subheader(school_name + "의 급식을 찾아볼게요!")
-        date = st.date_input("조회할 날짜를 입력해주세요:", datetime.date(2024, 4, 2)).strftime("%Y%m%d")
-
-        # 2. 급식 검색하기
-        menu = give_me_meal(district_code, school_code, date)
-        if menu:
-            st.table(menu)
-    else:
-        st.warning("해당 이름의 학교를 찾을 수 없습니다. 다시 입력해주세요.")
-else:
-    st.info("학교 이름을 입력하고 조회 버튼을 눌러주세요.")
